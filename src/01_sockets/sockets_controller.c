d8d20ad66c2a53573ae18aca05b6d0b5b2dbf772
William Brumble <W.Brumble@gmail.com>
2021-03-14 15:28:27 -0500

    work has been done

--- src/01_sockets/sockets_controller.c
+++ src/01_sockets/sockets_controller.c
@@ -1,8 +1,58 @@
+#include <stdlib.h>
+
 #include "sockets_controller.h"
 #include "tcp_server.h"
 
-int socket_controller() {    
-    test();
+/* Determines if first time running. Set the default to true. */
+//one_first_scan = 1;
+
+/* Controls all socket related functionality. */
+int
+socket_controller()
+{
+	/* If this is the first time running, let's initialize. */
+	if(one_first_scan == 1)
+	{
+		/* Change variable to indicate program has been ran */
+		one_first_scan = 0;
+
+		/* Perform first time running layer one initialization */
+		if(one_initialize() == -1) { return -1; }
+	}
+
+	// test();
+
     return 0;
 }
 
+int
+one_initialize()
+{
+	/* Buffers to store data slated for sending. */
+	one_send_buff_one 	 = (char *) malloc(65535);
+	if(one_send_buff_one == NULL) { return -1; }
+
+	one_send_buff_two 	 = (char *) malloc(65535);
+	if(one_send_buff_two == NULL) { return -1; }
+
+	one_send_buff_three	 = (char *) malloc(65535);
+	if(one_send_buff_three == NULL) { return -1; }
+
+	one_send_buff_four   = (char *) malloc(65535);
+	if(one_send_buff_four == NULL) { return -1; }
+
+	/* Buffers to store data slated for receiving */
+	one_recv_buff_one 	 = (char *) malloc(65535);
+	if(one_recv_buff_one == NULL) { return -1; }
+
+	one_recv_buff_two 	 = (char *) malloc(65535);
+	if(one_recv_buff_one == NULL) { return -1; }
+
+	one_recv_buff_three  = (char *) malloc(65535);
+	if(one_recv_buff_one == NULL) { return -1; }
+
+	one_recv_buff_four 	 = (char *) malloc(65535);
+	if(one_recv_buff_one == NULL) { return -1; }
+
+	return 0;
+}
--- src/01_sockets/sockets_controller.h
+++ src/01_sockets/sockets_controller.h
@@ -1,6 +1,25 @@
 #ifndef SOCKET_CONTROLLER_H
 #define SOCKET_CONTROLLER_H
 
+/* Controls all socket related functionality. */
 int socket_controller();
+
+/* Determines if first time running. */
+_Bool one_first_scan = 1;
+
+/* Performs first time running initialization */
+int one_initialize();
+
+/* Buffers to store data slated for sending. */
+char *one_send_buff_one;
+char *one_send_buff_two;
+char *one_send_buff_three;
+char *one_send_buff_four;
+
+/* Buffers to store data slated for receiving. */
+char *one_recv_buff_one;
+char *one_recv_buff_two;
+char *one_recv_buff_three;
+char *one_recv_buff_four;
 
 #endif /* SOCKET_CONTROLLER_H */
